639864498b1895b5f0a720883a9041625d4eb3ce
TheAncientOwl <dele.alex.o.o@gmail.com>
2025-09-05 21:45:42 +0300

    Add youtube video embed

--- src/components/SongRenderer/SongResource.tsx
+++ src/components/SongRenderer/SongResource.tsx
@@ -6,18 +6,41 @@
  *
  * @file SongResource.tsx
  * @author Alexandru Delegeanu
- * @version 0.1
+ * @version 0.2
  * @description Display song resource data.
  */
 
 import type { TResource } from '@/types/song.types';
-import { Flex, Icon, Link, ListItem, Text } from '@chakra-ui/react';
+import { AspectRatio, Box, Flex, Icon, Link, ListItem, Text } from '@chakra-ui/react';
 import { IoMdMusicalNote } from 'react-icons/io';
 
 type TSongResourceProps = TResource;
 
+const getYouTubeVideoId = (url: string): string | null => {
+  try {
+    const parsedUrl = new URL(url);
+    if (
+      parsedUrl.hostname === 'www.youtube.com' ||
+      parsedUrl.hostname === 'youtube.com' ||
+      parsedUrl.hostname === 'm.youtube.com'
+    ) {
+      if (parsedUrl.pathname.startsWith('/shorts/')) {
+        return parsedUrl.pathname.split('/')[2];
+      }
+      return parsedUrl.searchParams.get('v');
+    }
+    if (parsedUrl.hostname === 'youtu.be') {
+      return parsedUrl.pathname.slice(1);
+    }
+    return null;
+  } catch {
+    return null;
+  }
+};
+
 export const SongResource = (props: TSongResourceProps) => {
-  console.log(props.link);
+  const videoId = getYouTubeVideoId(props.link);
+
   return (
     <ListItem>
       <Flex direction='column' alignItems='center' textAlign='center'>
@@ -30,6 +53,19 @@ export const SongResource = (props: TSongResourceProps) => {
         <Text fontStyle='italic' textAlign='center' fontSize={['small', 'sm']}>
           by {props.author}
         </Text>
+
+        {videoId ? (
+          <Box width='100%' maxW='400px' my={2}>
+            <AspectRatio ratio={16 / 9}>
+              <iframe
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title={props.alias}
+                allow='accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                allowFullScreen
+              />
+            </AspectRatio>
+          </Box>
+        ) : null}
       </Flex>
     </ListItem>
   );
