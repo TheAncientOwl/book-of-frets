d1f21e5daef412161cae44ff7c68130d5cbbebfb
TheAncientOwl <dele.alex.o.o@gmail.com>
2025-09-14 16:12:40 +0300

    Add webpify tool

---
+++ tools/webpify/install_deps.sh
@@ -0,0 +1,30 @@
+# !/bin/bash
+# -----------------------------------------------------------------------------
+#                     Copyright (c) by BookOfFrets 2025
+# -----------------------------------------------------------------------------
+# @license https://github.com/TheAncientOwl/book-of-frets/blob/main/LICENSE
+#
+# @file install_deps.sh
+# @author Alexandru Delegeanu
+# @version 0.1
+# @description Install dependencies for webpify tool
+#
+
+if [ -z "$BOOK_OF_FRETS_ROOT" ]; then
+  echo "Error: BOOK_OF_FRETS_ROOT is not set."
+  exit 1
+fi
+
+home=$BOOK_OF_FRETS_ROOT/tools/webpify
+
+cd $home
+
+if [ ! -d ".venv" ]; then
+  python3 -m venv "$home/.venv"
+fi
+
+source "$home/.venv/bin/activate"
+
+pip3 install -r requirements.txt
+
+cd -
