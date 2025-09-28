9f3bff34a0a9808ab701f3010dc5d55521389855
TheAncientOwl <dele.alex.o.o@gmail.com>
2025-09-28 15:19:58 +0300

    [Tooling] Add cover sizes generator

---
+++ project/tools/song-covers-generator/README.md
@@ -0,0 +1,17 @@
+# 🛠️ 🏞️ Songs Covers Generator
+
+Generates necessary images for song cover images used by cards / song UI components.
+
+## Usage
+
+```bash
+$ ./project/tools/song/covers/generator/run.sh <input_image_path>
+```
+
+## Notes
+
+In order for the scripts to work `$BOOK_OF_FRETS` env variable has to be set to the root of project directory.
+
+```
+$ export $BOOK_OF_FRETS="path/to/project/root"
+```
---
+++ project/tools/song-covers-generator/run.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+# -----------------------------------------------------------------------------
+#                     Copyright (c) by BookOfFrets 2025
+# -----------------------------------------------------------------------------
+# @license https://github.com/TheAncientOwl/book-of-frets/blob/main/LICENSE
+#
+# @file run.sh
+# @author Alexandru Delegeanu
+# @version 0.1
+# @description Generate necessary cover image sizes
+#
+
+
+if [ -z "$BOOK_OF_FRETS_ROOT" ]; then
+  echo "Error: BOOK_OF_FRETS_ROOT is not set."
+  exit 1
+fi
+
+$BOOK_OF_FRETS_ROOT/project/tools/image-resizer/run.sh "$1" 64 64
+$BOOK_OF_FRETS_ROOT/project/tools/image-resizer/run.sh "$1" 128 128
+$BOOK_OF_FRETS_ROOT/project/tools/image-resizer/run.sh "$1" 192 192
