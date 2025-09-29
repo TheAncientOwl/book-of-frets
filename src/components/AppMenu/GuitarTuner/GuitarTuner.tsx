7e6ae762940553feb159753f85f69f9cc41db1be
TheAncientOwl <dele.alex.o.o@gmail.com>
2025-09-29 21:20:03 +0300

    Clear active string on tuner stop

--- src/components/AppMenu/GuitarTuner/GuitarTuner.tsx
+++ src/components/AppMenu/GuitarTuner/GuitarTuner.tsx
@@ -6,7 +6,7 @@
  *
  * @file GuitarTuner.tsx
  * @author Alexandru Delegeanu
- * @version 0.2
+ * @version 0.3
  * @description Main tuner component.
  */
 
@@ -16,9 +16,9 @@ import { FaPlay } from 'react-icons/fa6';
 import { Box, IconButton } from '@chakra-ui/react';
 
 import { usePitchDetector, type TNotesConfiguration } from '@/common/hooks/usePitchDetector';
+import { FrequencyProgressIndicator } from '@/components/AppMenu/GuitarTuner/FrequencyProgressIndicator';
 import { Strings, type TStringName } from '@/components/AppMenu/GuitarTuner/Strings';
 import { FaStop } from 'react-icons/fa';
-import { FrequencyProgressIndicator } from '@/components/AppMenu/GuitarTuner/FrequencyProgressIndicator';
 
 const StandardNotes: TNotesConfiguration = [
   { name: 'E2', freq: 82.41 },
@@ -43,6 +43,11 @@ export const GuitarTuner = () => {
 
   const [activeString, setActiveString] = useState<TStringName | null>(null);
 
+  const stopTuner = () => {
+    pitchDetector.stopTuner();
+    setActiveString(null);
+  };
+
   return (
     <Box
       textAlign='center'
@@ -52,7 +57,7 @@ export const GuitarTuner = () => {
       <IconButton
         aria-label='play-pause tuner'
         icon={pitchDetector.isRunning ? <FaStop /> : <FaPlay />}
-        onClick={pitchDetector.isRunning ? pitchDetector.stopTuner : pitchDetector.startTuner}
+        onClick={pitchDetector.isRunning ? stopTuner : pitchDetector.startTuner}
         mb='10px'
       />
 
