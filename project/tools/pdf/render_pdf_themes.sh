b6deb2bc5249b6957fca2c6e830e173cd2958176
TheAncientOwl <dele.alex.o.o@gmail.com>
2026-02-12 22:03:17 +0200

    Fix renderer script path

--- project/tools/pdf/render_pdf_themes.sh
+++ project/tools/pdf/render_pdf_themes.sh
@@ -6,7 +6,7 @@
 #
 # @file render_pdf_themes.sh
 # @author Alexandru Delegeanu
-# @version 1.0
+# @version 1.1
 # @description Runs @see make_pdf.py for a song with all themes
 #
 
@@ -22,9 +22,9 @@ config="$1"
 themes=("banana" "bright-sky" "bubble-gum" "catpuccin" "cherry" "hazbin" "lavander" "nature" "peach" "pop-n-lock")
 
 echo ">> Making \"$config\" with theme \"default-dark\""
-python3 project/tools/pdf/make_pdf.py "$config" --chords public/chords/index.json
+python3 project/tools/pdf/render.py "$config" --chords public/chords/index.json
 
 for theme in "${themes[@]}"; do
     echo ">> Making \"$config\" with theme \"$theme\""
-    python3 project/tools/pdf/make_pdf.py "$config" --chords public/chords/index.json --theme public/themes/"$theme"/config.json
+    python3 project/tools/pdf/render.py "$config" --chords public/chords/index.json --theme public/themes/"$theme"/config.json
 done
