7e57767b1083fc87b9cd76e5529469373fe99220
TheAncientOwl <dele.alex.o.o@gmail.com>
2026-02-14 18:20:35 +0200

    Disable booky completions

--- project/tools/_booky-completion.sh
+++ project/tools/_booky-completion.sh
@@ -1,27 +1,31 @@
-# booky tab completion
+# # booky tab completion
 
-_booky_completions()
-{
-  local cur prev opts
+# _booky_completions()
+# {
+#   local cur prev opts
 
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
+#   COMPREPLY=()
+#   cur="${COMP_WORDS[COMP_CWORD]}"
+#   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-  opts="--help \
-        --webpify \
-        --jpegify \
-        --resize \
-        --song \
-        --render-pdf \
-        --render-pdf-all \
-        --render-pdf-themes"
+#   opts="--help \
+#         --webpify \
+#         --jpegify \
+#         --resize \
+#         --song \
+#         --render-pdf \
+#         --render-pdf-all \
+#         --render-pdf-themes"
 
-  # First argument completion
-  if [ $COMP_CWORD -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-    return 0
-  fi
-}
+#   # First argument completion
+#   if [ $COMP_CWORD -eq 1 ]; then
+#     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+#     return 0
+#   fi
 
-complete -F _booky_completions booky.sh
+#   # Fallback to default file/path completion
+#   COMPREPLY=( $(compgen -f -- "${cur}") )
+#   return 0
+# }
+
+# complete -F _booky_completions booky.sh
